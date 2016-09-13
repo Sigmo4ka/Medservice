83774656799c4cfdc7d369390feceaa9a3e015e3
Sigmo4ka <sigm@ukr.net>
2016-09-14 01:20:24 +0300

    new files

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(".hidden-button").on("click", function() {
+		
+		$("nav ul").slideDown();
+		
+	});	// Кнопка появления скрытого меню
+
+
+// $(".hidden-button").on("click", function() {
+		
+// 		$("nav ul").slideToggle(2000, function() {
+// 			alert("hey");
+// 		});
+// 	});
